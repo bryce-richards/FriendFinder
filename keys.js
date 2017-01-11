39dd4488b5232460696acf13c69e7b8b8fb93e6b
Bryce Richards <brichards5@gmail.com>
2017-01-11 14:09:13 -0800

    added keys.js

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.credentials = {
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    port: 3306,
+    user: "be66145760d22c",
+    password: "b6a070bd",
+    database: "heroku_365d3b1ef23e18b"
+};
