f08ca68a0dc0262dc99c61b621b9a573256a7792
Bryce Richards <brichards5@gmail.com>
2017-01-11 13:49:42 -0800

    changed port

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,7 @@ var bluebird = require("bluebird");
 
 var connection = mysql.createConnection({
     host: "us-cdbr-iron-east-04.cleardb.net",
+    port: 3306,
     user: "be66145760d22c",
     password: "b6a070bd",
     database: "heroku_365d3b1ef23e18b"
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 5555;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
