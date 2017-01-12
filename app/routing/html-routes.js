6c51de29e089467aa23dfa6707272af2824e4507
Bryce Richards <brichards5@gmail.com>
2017-01-11 18:32:37 -0800

    removed favicon

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,14 +1,11 @@
 var path = require("path");
-var favicon = require("serve-favicon");
 
 module.exports = function (app) {
 
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
-
-    app.use(favicon(path.join(__dirname,"/../../handshake.png")));
-
+    
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
